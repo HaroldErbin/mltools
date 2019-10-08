7dce591452c662df4fbc34d9d24dd8071799f2ff
Harold Erbin <harold.erbin@gmail.com>
2019-10-09 01:05:33 +0200

    Get flexible samples for dict, array and dataframe

--- mltools/data/sample.py
+++ mltools/data/sample.py
@@ -6,10 +6,22 @@ import numpy as np
 import pandas as pd
 
 
-# TODO: samples based on different datasets
-# example: one dataset for both training and testing, another just for testing
+# TODO: sample with stratification
+# TODO: sample built from several datasets
 
-# TODO: samples with stratification
+# Question: 1) store dataset in RatioSample, or 2) the opposite,
+# or 3) make a mixed class?
+# 1) Allow to quickly get data with sample['train'], but mix two different
+#   ideas in a single class
+# 2) more difficult to handle several datasets at once, except if there is
+#   already a class to handle several datasets;
+#   problem: to ensure that several calls always give the same outputs, this
+#   requires to always use a Dataset class (for quick problem we may prefer
+#   to use directly a dataframe)
+#   (i.e. where to cache indices?)
+# 3) maybe too heavy
+# one possibily for the problem in 2) is to cache the indices in the instance
+# itself using a dict id(obj) → indices (but is it reliable?)
 
 
 class RatioSample:
@@ -26,19 +38,27 @@ class RatioSample:
     `val`, and `test`). All supplementary numbers are ignored. If the ratios
     of a list don't add to 1, then the different to 1 is added to the list.
     This helps to quickly change the number of training versus test data.
+
+    This class can extract samples from a dataset in two ways:
+    1. store dataset in attribute, get samples from keys
+    2. call the instance on
+    A dataset can be stored in an attribute either at initialisation, or by
+    calling the method `store_dataset`. In this case, the indices are cached
+    for efficiency and to ensure that the same subsets are obtained (if the
+    dataset does not change). The only consistency check which is performed
+    is that the length of the dataset does not change.
     """
 
-    # TODO: several datasets as arguments?
     # can decide to shuffle randomly between the different sets
 
-    # TODO: implement dict and array for dataset
-
-    def __init__(self, dataset, ratios):
+    def __init__(self, ratios, shuffle=False, dataset=None):
         """
-        :param dataset: data to split in subsets
-        :type dataset: `pandas.DataFrame`, `numpy.array`, `dict`
         :param ratios: ratios used to divide the data in subsets
         :type ratios: `float`, `list`, `tuple` or `dict`
+        :param dataset: data to split in subsets
+        :type dataset: `pandas.DataFrame`, `numpy.array`, `dict`
+        :param shuffle: shuffle the dataset before extracting samples
+        :type shuffle: `bool`
         """
 
         if not isinstance(ratios, (dict, tuple, list, float)):
@@ -70,13 +90,92 @@ class RatioSample:
             raise ValueError("The sum of ratios must be between 0 and 1, "
                              "found {}.".format(sum_ratios))
 
-        if not isinstance(dataset, (pd.DataFrame, np.ndarray, dict)):
-            raise TypeError("Dataset must be a dict, an array or dataframe.")
+        self.shuffle = shuffle
 
-        if isinstance(dataset, (np.ndarray, dict)):
-            raise NotImplementedError
+#        if dataset is not None:
+#            self.store_dataset(dataset, shuffle)
 
-        self.dataset = dataset
+    def __repr__(self):
+        return "<RatioSample: {}>".format(self.ratios)
 
-        # cache id for the different subsets
-        self._index_cache = []
+    def __call__(self, dataset, shuffle=False):
+        """
+        Split a set of id into subsets according to the class ratios.
+
+        :param dataset: data to split in subsets
+        :type dataset: `pandas.DataFrame`, `numpy.array`, `dict`
+        :return: subsets arranged in a `dict`
+        :rtype: `dict`
+        """
+
+        if isinstance(dataset, dict):
+            # for a dict, get number of samples by looking at the first
+            # key
+            size = len(list(dataset.values())[0])
+        elif isinstance(dataset, (pd.DataFrame, np.ndarray)):
+            # works only for array, dataframe
+            size = len(dataset)
+        else:
+            raise TypeError("Dataset type `{}` is not supported"
+                            .format(type(dataset)))
+
+        idx_splits = self.make_samples(size, shuffle)
+        value_splits = {}
+
+        for key, idx in idx_splits.items():
+            if isinstance(dataset, dict):
+                value_splits[key] = {}
+                for col, val in dataset.items():
+                        value_splits[key][col] = val[idx, ...]
+            elif isinstance(dataset, np.ndarray):
+                    value_splits[key] = dataset[idx, ...]
+            elif isinstance(dataset, pd.DataFrame):
+                    value_splits[key] = dataset.loc[list(idx)]
+
+        return value_splits
+
+    def make_samples(self, size, shuffle=False):
+        """
+        Split a set of id into subsets according to the class ratios.
+
+        :param size: size of the dataset
+        :type size: `int`
+        :return: indices for each subsets arranged in a `dict`
+        :rtype: `dict`
+        """
+
+        splits = {}
+        indices = range(0, size)
+
+        if shuffle is True:
+            indices = np.random.permutation(indices)
+
+        last = 0
+
+        for key, ratio in self.ratios.items():
+            delta = round(ratio * size)
+            splits[key] = tuple(indices[last:last+delta])
+            last += delta
+
+        # TODO: this assumes that the samples are indexed with consecutive
+        #   integers; this should work with more general id
+
+        return splits
+
+#    def store_dataset(self, dataset):
+#
+#        self.dataset = dataset
+#
+#        self._size = len(dataset)
+#        self._indices = self.make_samples(self._size)
+#
+#        if not isinstance(dataset, (pd.DataFrame, np.ndarray, dict)):
+#            raise TypeError("Dataset must be a dict, an array or dataframe.")
+#
+#        if isinstance(dataset, (np.ndarray, dict)):
+#            raise NotImplementedError
+#
+#        self.dataset = dataset
+#
+#        # cache id for the different subsets
+#        self._index_cache = []
--- tests/data/test_sample.py
+++ tests/data/test_sample.py
@@ -6,37 +6,112 @@ import pandas as pd
 from mltools.data.sample import RatioSample
 
 
-def test_ratiosample_definition_ratios():
+dic = {'id': np.arange(0, 10), 'letter': np.array(list('abcdefghij')),
+       'number': np.arange(20, 30)}
+df = pd.DataFrame(dic)
+array = df[['id', 'number']].values
 
-    df = pd.DataFrame()
 
-    assert RatioSample(df, 0.2).ratios == {"train": 0.2, "test": 0.8}
-    assert RatioSample(df, [0.2]).ratios == {"train": 0.2, "test": 0.8}
+def test_ratiosample_def_ratios_numbers():
 
-    assert RatioSample(df, [0.2, 0.8]).ratios == {"train": 0.2, "test": 0.8}
-    assert (RatioSample(df, [0.2, 0.3]).ratios
+    assert RatioSample(0.2).ratios == {"train": 0.2, "test": 0.8}
+    assert RatioSample([0.2]).ratios == {"train": 0.2, "test": 0.8}
+
+    assert RatioSample([0.2, 0.8]).ratios == {"train": 0.2, "test": 0.8}
+    assert (RatioSample([0.2, 0.3]).ratios
             == {"train": 0.2, "val": 0.3, "test": 0.5})
 
-    assert (RatioSample(df, [0.2, 0.3, 0.4]).ratios
+    assert (RatioSample([0.2, 0.3, 0.4]).ratios
             == {"train": 0.2, "val": 0.3, "test": 0.4})
 
-    assert (RatioSample(df, [0.1, 0.2, 0.3, 0.3]).ratios
+    assert (RatioSample([0.1, 0.2, 0.3, 0.3]).ratios
             == {"train": 0.1, "val": 0.2, "test": 0.3})
-    assert (RatioSample(df, [0.1, 0.2, 0.3, 0.4]).ratios
+    assert (RatioSample([0.1, 0.2, 0.3, 0.4]).ratios
             == {"train": 0.1, "val": 0.2, "test": 0.3})
+
+
+def test_ratiosample_def_ratios_dict():
+
+    assert (RatioSample({"train": 0.2, "val": 0.3, "test": 0.4}).ratios
+            == {"train": 0.2, "val": 0.3, "test": 0.4})
+    assert (RatioSample({"train": 0.2, "val": 0.3, "test": 0.5}).ratios
+            == {"train": 0.2, "val": 0.3, "test": 0.5})
+
+    assert (RatioSample({"train-nn": 0.1, "train-reg": 0.2, "val": 0.3,
+                         "test": 0.4}).ratios
+            == {"train-nn": 0.1, "train-reg": 0.2, "val": 0.3, "test": 0.4})
 
 
 def test_ratiosample_sum_ratios_error():
 
-    df = pd.DataFrame()
-
     with pytest.raises(ValueError):
-        RatioSample(df, [0.2, 0.9])
+        RatioSample([0.2, 0.9])
 
 
-def test_ratiosample_notimplemented_errors():
-    with pytest.raises(NotImplementedError):
-        RatioSample(np.array([(0, 10), (1, 20)]), 0.4)
+def test_ratiosample_make_samples():
 
-    with pytest.raises(NotImplementedError):
-        RatioSample({"id": [0, 1], "value": [10, 20]}, 0.4)
+    assert (RatioSample([0.2]).make_samples(10)
+            == {'train': (0, 1), 'test': (2, 3, 4, 5, 6, 7, 8, 9)})
+
+    assert (RatioSample([0.2, 0.3]).make_samples(10)
+            == {'train': (0, 1), 'val': (2, 3, 4), 'test': (5, 6, 7, 8, 9)})
+
+
+def test_ratiosample_make_samples_rounding():
+    r = RatioSample([0.25, 0.3])
+    assert sum(map(len, r.make_samples(1500).values())) == 1500
+
+    r = RatioSample([0.25, 0.23])
+    assert sum(map(len, r.make_samples(1500).values())) == 1500
+
+    r = RatioSample({"train": 0.19, "test": 0.56})
+    assert sum(map(len, r.make_samples(2000).values())) == 1500
+
+
+def test_ratiosample_call_array():
+
+    sample = RatioSample([0.2, 0.3])
+
+    array_true = {'train': np.array(((0, 20), (1, 21))),
+                  'val': np.array(((2, 22), (3, 23), (4, 24))),
+                  'test': np.array(((5, 25), (6, 26), (7, 27), (8, 28),
+                                    (9, 29)))}
+    array_res = sample(array)
+
+    for k in array_true:
+        np.testing.assert_array_equal(array_res[k], array_true[k])
+
+
+def test_ratiosample_call_dataframe():
+
+    sample = RatioSample([0.2, 0.3])
+
+    df_true = {'train': df.loc[[0, 1]],
+               'val': df.loc[[2, 3, 4]],
+               'test': df.loc[[5, 6, 7, 8, 9]]}
+
+    df_res = sample(df)
+
+    for k in df_true:
+        pd.testing.assert_frame_equal(df_res[k], df_true[k])
+
+
+def test_ratiosample_call_dic():
+
+    sample = RatioSample([0.2, 0.3])
+
+    dic_true = {'train': {'id': np.array([0, 1]),
+                          'letter': np.array(['a', 'b']),
+                          'number': np.array([20, 21])},
+                'val': {'id': np.array([2, 3, 4]),
+                          'letter': np.array(['c', 'd', 'e']),
+                          'number': np.array([22, 23, 24])},
+                'test': {'id': np.array([5, 6, 7, 8, 9]),
+                          'letter': np.array(['f', 'g', 'h', 'i', 'j']),
+                          'number': np.array([25, 26, 27, 28, 29])}}
+
+    dic_res = sample(dic)
+
+    for k1 in dic_true:
+        for k2 in dic:
+            np.testing.assert_array_equal(dic_true[k1][k2], dic_res[k1][k2])
